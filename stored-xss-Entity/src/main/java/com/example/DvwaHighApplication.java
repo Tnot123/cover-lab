7db3761578d6b7f55b331ce6e7d153e3e551d09c
Tnot123 <123921939+Tnot123@users.noreply.github.com>
2025-08-15 22:56:23 +0700

    Add files via upload

---
+++ stored-xss-Entity/src/main/java/com/example/DvwaHighApplication.java
@@ -0,0 +1,11 @@
+package com.example.dvwahigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvwaHighApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DvwaHighApplication.class, args);
+    }
+}
